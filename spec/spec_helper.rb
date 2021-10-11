3424ac9d9420b123d44c79da5399d3aa46b269d8
FrancineBlanc <francine.blanc@gmail.com>
2021-10-11 15:47:25 +0100

    adds simplecov in helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,13 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  SimpleCov::Formatter::HTMLFormatter
+])
+
+SimpleCov.start
+
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
